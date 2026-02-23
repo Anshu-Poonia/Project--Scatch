f76c7ba2416511093e7105dd86fdb88fe6eb9621
Anshu Poonia <anshupoonia902005@gmail.com>
2026-02-24 00:02:50 +0530

    Clarify variable extraction in product creation route
    Updated product creation route to clarify variable extraction.

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const upload = require("../config/multer-config");
 const productModel = require("../models/product-model");
 
+// creating a product by the admin and stored in the database with these details and display product on the user panel
 router.post("/create", upload.single("image"), async (req, res) => {
     try {
-        let { name, price, discount, bgcolor, panelcolor, textcolor } = req.body;
+        let { name, price, discount, bgcolor, panelcolor, textcolor } = req.body;   // info provided by the admin about product and we access it from the body
 
+        // creating the product
         let product = await productModel.create({
             name,
             price,
